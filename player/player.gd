103f6fe5836258926c933a3e614d6e2ebcb1bf7c
array-in-a-matrix <git@arrayinamatrix.xyz>
2023-09-28 11:31:52 -0400

    changed file structure

--- player/player.gd
+++ player/player.gd
@@ -2,13 +2,13 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	velocity = Vector2()
-	if Input.is_key_pressed(KEY_A):
+	if Input.is_key_pressed(KEY_A) || Input.is_key_pressed(KEY_LEFT):
 		velocity.x -= 1
-	if Input.is_key_pressed(KEY_W):
+	if Input.is_key_pressed(KEY_W) || Input.is_key_pressed(KEY_UP):
 		velocity.y -= 1
-	if Input.is_key_pressed(KEY_S):
+	if Input.is_key_pressed(KEY_S) || Input.is_key_pressed(KEY_DOWN):
 		velocity.y += 1
-	if Input.is_key_pressed(KEY_D):
+	if Input.is_key_pressed(KEY_D) || Input.is_key_pressed(KEY_RIGHT):
 		velocity.x += 1
 	velocity *= 300
 	
