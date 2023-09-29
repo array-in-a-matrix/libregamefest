9a20c08c924aca8fe35ee40d43d49ec828053b89
array-in-a-matrix <git@arrayinamatrix.xyz>
2023-09-29 12:39:03 -0400

    added player char animated idle sprite

--- player/player.gd
+++ player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var animated_sprite: AnimatedSprite2D = $animated_sprite_2d
+
 func _physics_process(delta):
 	velocity = Vector2()
 	if Input.is_key_pressed(KEY_A) || Input.is_key_pressed(KEY_LEFT):
@@ -10,6 +12,10 @@ func _physics_process(delta):
 		velocity.y += 1
 	if Input.is_key_pressed(KEY_D) || Input.is_key_pressed(KEY_RIGHT):
 		velocity.x += 1
-	velocity *= 300
+	velocity *= 150
 	
 	move_and_slide()
+	update_animation()
+
+func update_animation():
+	animated_sprite.play("idle")
